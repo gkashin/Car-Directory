216f3c19227997fc4c267aaa613e786cdbec77f2
George <georgii.kashin@gmail.com>
2019-10-13 11:39:10 +0300

    Initial Commit

---
+++ CarDirectory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarDirectory
+//
+//  Created by Георгий Кашин on 13.10.2019.
+//  Copyright © 2019 Georgii Kashin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
